a1e26a88dbd002e1cc5abca16aa802dcd934a375
sanbgok.lee <fx.isbok99@gmail.com>
2024-01-11 18:23:06 +0900

    Added score board

--- FindCat/find_cat_v1.py
+++ FindCat/find_cat_v1.py
@@ -1,10 +1,15 @@
 import pathlib
 import re
+import time
 
 class FindCat:
-    def __init__(self, s_path: str):
-        self.s_path = f'{pathlib.Path.cwd()}/{s_path}'
-        print(self.s_path)
+    def __init__(self):
+        with open(f'{pathlib.Path.cwd()}/list.txt','r') as d_list:
+            print([l.rstrip() for l in d_list])
+
+        self.i_path = input('어디로 가시겠습니까? : ')
+        self.s_path = f'{pathlib.Path.cwd()}/{self.i_path}'
+        self.life = 3
         pass
 
     def search(self):
@@ -12,20 +17,38 @@ class FindCat:
         s_list = list(s_list.glob('*.txt'))
         pattern = 'cat'
 
-        for i in s_list:
-            result = re.search(pattern, i.name)
-            if (result == None):                    #state: failed to find
-                print('Unfound')
-                continue
+        for i in s_list:                        #s_list is file list in s_path
+            f_path = f'{self.s_path}/{i.name}'  # print *.txt
+            with open(f_path, 'r') as f_txt:
+                for j in f_txt.readlines():
+                    print(j.rstrip())
+                    result = re.search(pattern,i.name)
+            if (result==None):                            #state: failed to find
+                self.life -= 1
+                print('\n당신은 고양이를 찾지 못했습니다...\n남은 목숨 :', '\u2665'*self.life)
             else:
-                f_path = f'{self.s_path}/{i.name}'  #state: success to find
-                with open(f_path,'r') as f_txt:
-                    for i in f_txt.readlines():
-                        print(i.rstrip('\n'))            # .strip() is function which delete '\n' at last
+                print('\n축하합니다! 당신은 고양이를 찾았습니다!\n당신은',(3-self.life)+1,'번 만에 성공했군요!')
+
+                score_path = f'{pathlib.Path.cwd()}/score_board.txt'
+                with open(score_path,'a') as r_list:
+                    usr_name = input('\n당신의 이름은? : ')
+                    r_list.write(f'\n{usr_name} = {self.life}')
+
+                with open(score_path,'r') as scoreboard:
+                    sb_lst = scoreboard.readlines()
+
+                    #clamping delay
+                    delay = 10/len(sb_lst)
+                    if (delay>0.5): delay = 0.5
+                    elif (delay<0.25): delay = 0.25
+
+                    #print
+                    for k in sb_lst:
+                        print(k.rstrip())
+                        time.sleep(delay)
 
 
-
-a = FindCat('a/b')
+a = FindCat()
 print(a.search())
 
 
